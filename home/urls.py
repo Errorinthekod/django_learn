8aba4a089f1e7aa73300ec89793ae6109cefa31c
Errorinthekod <ashataskerbekov6@gmail.com>
2026-08-24 10:08:45 +0600

    dynamic urls: id, slug, re_path, regex urls, converters

--- home/urls.py
+++ home/urls.py
@@ -1,6 +1,13 @@
-from django.urls import path
-from home.views import first_view
+from django.urls import path, re_path, register_converter
+from . import views
+from . import converters
+
+register_converter(converters.FourDigitYearConverter, "year4")
 
 urlpatterns = [
-    path("view/", first_view),
+    path("view/", views.first),
+    path("dynamic_view/<int:cat_id>/", views.dynamic),
+    path("slug_view/<slug:cat_slug>/", views.slug),
+    # re_path(r"^archive/(?P<year>[0-9]{4})/", views.archive)
+    path("archive/<year4:year>/", views.archive)
 ]
--- home/views.py
+++ home/views.py
@@ -3,5 +3,17 @@ from django.shortcuts import render
 
 # Create your views here.
 
-def first_view(request):
+def first(request):
     return HttpResponse("First view")
+
+
+def dynamic(request, cat_id):
+    return HttpResponse(f"<h1>Hello from dynamic view</h1><p>id: {cat_id}</p>")
+
+
+def slug(request, cat_slug):
+    return HttpResponse(f"<h1>Hello from slug view</h1><p>slug: {cat_slug}</p>")
+
+
+def archive(request, year):
+    return HttpResponse(f"<h1>Hello from archive view</h1><p>year: {year}</p>")
