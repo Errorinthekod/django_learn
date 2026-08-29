70a11713ae4a424f1a014df35e35b00b3549d9df
Errorinthekod <ashataskerbekov6@gmail.com>
2026-08-29 16:44:24 +0600

    template tags if and for

--- home/urls.py
+++ home/urls.py
@@ -6,6 +6,9 @@ register_converter(converters.FourDigitYearConverter, "year4")
 
 urlpatterns = [
     path("", views.home, name = "home"),
+    path("about/", views.about, name = "about"),
+    path("login/", views.login, name = "login"),
+    path("contact/", views.contact, name = "contact"),
     path("view/", views.first, name = "first"),
     path("dynamic_view/<int:cat_id>/", views.dynamic, name = "dynamic_id"),
     path("slug_view/<slug:cat_slug>/", views.slug, name = "slug"),
@@ -14,4 +17,7 @@ urlpatterns = [
     path("archive/<year4:year>/", views.archive, name = "archive"),
     path("rndr_to_str/", views.rndr_to_str, name = "render_to_string"),
     path("rndr_html/", views.rndr_html, name = "render_html"),
+    path("goods/", views.dataDB, name = "goods_html"),
+    path("goods_id/<int:goods_id>/", views.show_goods, name = "show_goods_html"),
+
 ]
--- home/views.py
+++ home/views.py
@@ -1,15 +1,27 @@
 from django.http import HttpResponse, HttpResponseNotFound, Http404, HttpResponseRedirect, HttpResponsePermanentRedirect
 from django.shortcuts import render, redirect
 from django.urls import reverse
+from django.template.defaultfilters import slugify
 from django.template.loader import render_to_string
 
 
 menu = ["Main page", "About", "Login"]
+main_menu = [
+    {"name": "Login", "url_name": "login"},
+    {"name": "Contact", "url_name": "contact"},
+    {"name": "About", "url_name": "about"},
+]
 
 class MyClass:
     def __init__(self, a, b):
         self.a = a
         self.b = b
+
+data_db = [
+    {"id": 1, "title": "Goods 1", "description": "The bad goods", "is_active": True},
+    {"id": 2, "title": "Goods 2", "description": "The good goods", "is_active": False},
+    {"id": 3, "title": "Goods 3", "description": "The good goods", "is_active": True}
+]
 
 def first(request):
     return HttpResponse("First view")
@@ -79,6 +91,29 @@ def rndr_html(request):
             "k2": "v2",
         },
         "obj": MyClass(10, 20),
+        "url": slugify("The main page"),
 
     }
     return render(request, "home/index.html", context=data)
+
+def dataDB(request):
+    data = {
+        "main_menu": main_menu,
+        "goods": data_db,
+
+    }
+    return render(request, "home/goods.html", context = data)
+
+
+def show_goods(request, goods_id):
+    return HttpResponse(f"Goods with id:{goods_id}")
+
+def about(request):
+    return HttpResponse("About")
+
+def login(request):
+    return HttpResponse("Login")
+
+def contact(request):
+    return HttpResponse("pywask")
+
