bf52d76fa0e90cabe0b5012be17342f325fdd393
Errorinthekod <ashataskerbekov6@gmail.com>
2026-09-14 00:13:42 +0600

    final modifyings in models for vacancies

--- vacancy/models.py
+++ vacancy/models.py
@@ -6,6 +6,7 @@ from django.db import models
 class Category(models.Model):
     name = models.CharField(max_length=100, verbose_name="Category Name", unique=True)
     description = models.TextField(verbose_name="Category Description", null=True, blank=True)
+
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True, null=True, blank=True)
     is_active = models.BooleanField(default=True, verbose_name="Category Status")
@@ -21,6 +22,7 @@ class Category(models.Model):
 class Tag(models.Model):
     name = models.CharField(max_length=100, verbose_name="Tag Name", unique=True)
     description = models.TextField(verbose_name="Tag Description", null=True, blank=True)
+
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True, null=True, blank=True)
     is_active = models.BooleanField(default = True, verbose_name = "Tag Status")
@@ -40,6 +42,7 @@ class Source(models.Model):
     def __str__(self):
         return self.name
 
+
 class Vacancy(models.Model):
 
     class TypeChoices(models.TextChoices):
@@ -54,11 +57,11 @@ class Vacancy(models.Model):
         SENIOR      = "SR", "Senior"
         ARCHITECT   = "AR", "Architect"
 
-    title = models.CharField(max_length=100, verbose_name="Vacancy Title", blank=True)
-    position = models.CharField(max_length=100, verbose_name="Vacancy Position", blank=True)
-    company = models.CharField(max_length=100, verbose_name="Vacancy Company", blank=True)
-    experience = models.PositiveIntegerField(default=0, verbose_name="Vacancy Experience")
-    type = models.CharField(
+    title = models.CharField(max_length=100, verbose_name="Vacancy Title")
+    company = models.CharField(max_length=100, verbose_name="Vacancy Company")
+    position = models.CharField(max_length=100, verbose_name="Vacancy Position")
+
+    work_type = models.CharField(
         max_length=2,
         choices=TypeChoices.choices,
         verbose_name="Vacancy Type"
@@ -69,18 +72,22 @@ class Vacancy(models.Model):
         blank=True,
         verbose_name="Vacancy Grade"
     )
-    salary = models.PositiveBigIntegerField(verbose_name="Vacancy Salary", null=True, blank=True)
+    salary_min = models.PositiveBigIntegerField(null=True, blank=True, verbose_name="Vacancy Salary")
+    salary_max = models.PositiveBigIntegerField(null=True, blank=True, verbose_name="Vacancy Salary")
+    salary_currency = models.CharField(max_length=3, blank=True, verbose_name="Vacancy Salary Currency")
+    experience_min = models.PositiveIntegerField(null=True, blank=True, verbose_name="Vacancy Experience Min")
+    experience_max = models.PositiveIntegerField(null=True, blank=True, verbose_name="Vacancy Experience Max")
 
-    responsibilities = models.TextField(verbose_name="Vacancy Responsibilities", blank=True)
-    requirements = models.TextField(verbose_name="Vacancy Requirements", blank=True)
-    registration = models.CharField(max_length=100, verbose_name="Vacancy Registration", blank=True)
-    employment = models.CharField(max_length=100, verbose_name="Vacancy Employment", blank=True)
-    priorities = models.TextField(verbose_name="Vacancy Priorities", blank=True)
+    priorities = models.TextField(blank=True, verbose_name="Vacancy Priorities")
+    requirements = models.TextField(blank=True, verbose_name="Vacancy Requirements")
+    responsibilities = models.TextField(blank=True, verbose_name="Vacancy Responsibilities")
+    employment = models.CharField(max_length=100, blank=True, verbose_name="Vacancy Employment")
+    reg_method = models.CharField(max_length=100, blank=True, verbose_name="Vacancy Registration")
 
-    address = models.CharField(max_length=255, verbose_name="Vacancy Address", blank=True)
-    email = models.EmailField(verbose_name="Vacancy Email", blank=True)
-    phone = models.CharField(max_length=100, verbose_name="Vacancy Phone", blank=True)
-    telegram = models.CharField(max_length=200, verbose_name="Vacancy Telegram", blank=True)
+    email = models.EmailField(blank=True, verbose_name="Vacancy Email")
+    phone = models.CharField(max_length=100, blank=True, verbose_name="Vacancy Phone")
+    telegram = models.CharField(max_length=200, blank=True, verbose_name="Vacancy Telegram")
+    location = models.CharField(max_length=255, blank=True, verbose_name="Vacancy Location")
 
     category = models.ForeignKey(
         Category,
@@ -97,6 +104,7 @@ class Vacancy(models.Model):
         on_delete=models.PROTECT,
         related_name="vacancies",
         verbose_name="Vacancy Source")
+    source_url = models.URLField(unique=True, verbose_name="Vacancy Source URL")
 
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
